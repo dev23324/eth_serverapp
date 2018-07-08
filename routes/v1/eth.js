7307f791c8f6c2663209644bcc12891367999b51
zeljkokuruc <k.zeljko727@gmail.com>
2018-07-08 16:42:00 +0200

    eth 1 file

--- routes/v1/eth.js
+++ routes/v1/eth.js
@@ -3,5 +3,9 @@ var router = express.Router();
 var ethController = require('../../controllers/ethController');
 
 router.get('/tx/receipt/:tx_hash', ethController.transactionReceipt);
+router.get('/balance/:contract/:account', ethController.tokenBalance);
+router.get('/create', ethController.create);
+router.post('/sendTransaction', ethController.sendTransaction);
+router.post('/transferToken', ethController.transferToken);
 
 module.exports = router;
